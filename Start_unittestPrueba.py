ccb537c8d2cc20a9266d906dd3f4aa31380b2396
Karina <kfjaramillo.puce.edu.ec>
2024-07-21 19:41:13 -0500

    Prueba StartUnittest

--- Start_unittestPrueba.py
+++ Start_unittestPrueba.py
@@ -1,26 +1,42 @@
+#convertir a unittest
+import unittest
 #importar librerías: webdriver(librería principal para controlar el navegador) y by (localización de elementos en la web ID, nombre, clase,..)
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#crear una nueva instancia del navegador Microsoft Edge
-driver = webdriver.Edge() 
+#heredar de unittest clase
+class FindbyIdName(unittest.TestCase):
+    # crear 2 funciones Python inicio y fin de pruebas
+    # metodo de inicio de prueba. Usar driver de Selenium en navegador 
+    def setUp(self):
 
-#navegar a una web
-driver.get("http://www.clouditeducation.com/pruebas")
+    #crear una nueva instancia del navegador Microsoft Edge
+        self.driver = webdriver.Edge() 
 
+    #navegar a una web
+        self.driver.get("https://kfjaramillo.github.io/PaginaPruebas/")
+
+#Agregar un método las validaciones realizadas 
+    def testIdName(self):
 #busca un elemento en la página, en este caso ID cuyo atributo sea noImportante
-elemento = driver.find_element(By.ID, "noImportante")
+        elemento = self.driver.find_element(By.ID, "noImportante")
 
-#verifica si el elemento fue encontrado
-if elemento is not None:
-    print ("El elemento by ID fue encontrado")
+        #verifica si el elemento fue encontrado
+        if elemento is not None:
+            print ("El elemento by ID fue encontrado")
 
-#busca el elemento cuyo atributo NAME sea ultimo 
-elemento2 = driver.find_element(By.NAME, "ultimo")
+        #busca el elemento cuyo atributo NAME sea ultimo 
+        elemento2 = self.driver.find_element(By.NAME, "ultimo")
 
-#verifica si el elemento fue encontrado
-if elemento2 is not None:
-    print ("El elemento by NAME fue encontrado")
+        #verifica si el elemento fue encontrado
+        if elemento2 is not None:
+            print ("El elemento by NAME fue encontrado")
 
+    # cerrar el driver 
+    def tearDown(self):
+        self.driver.quit()
+# no es necessaria pero recomendable, asegurarse para asegurarse que es prueba independiente de este modulo
+if _name_=="_master_":
+    unittest.main*()
 #cierra el navegador    
-driver.quit()
+        
